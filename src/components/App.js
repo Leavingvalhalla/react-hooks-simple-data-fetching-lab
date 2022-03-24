5bd7c1e2f582c6c9d23668aecc3c9c75fbc51335
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-23 19:45:04 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={image} alt="A Random Dog" />;
+}
+
+export default App;
